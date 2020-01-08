3a1c90461d27a0d8b41a923f58dc5277490b00b8
sorattorafa <soratto@alunos.utfpr.edu.br>
2020-01-08 17:00:07 -0300

    test password

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
  class LoginRouter{ 
     route(httpRequest){ 
-        if(!httpRequest.body.email){ 
+        if(!httpRequest.body.email || !httpRequest.body.password){ 
             return{ 
                 statusCode: 400
             }
@@ -8,7 +8,7 @@
     }
 } 
 describe('Login router', () => {  
-    test('Should return 400 if no email is provided', async () => {
+    test('Should return 400 if no password is provided', async () => {
         const sut = new LoginRouter() 
         const httpRequest = {
           body: {
@@ -17,5 +17,16 @@ describe('Login router', () => {
         }
         const httpResponse = await sut.route(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
-      })
+      }) 
+
+      test('Should return 400 if no email is provided', async () => {
+        const sut = new LoginRouter() 
+        const httpRequest = {
+          body: {
+            email: 'any_email'
+          }
+        }
+        const httpResponse = await sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+      })  
 })

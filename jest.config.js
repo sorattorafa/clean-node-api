aab7f684b782bbad1e5b1515eb6517fb0719feb6
sorattorafa <soratto@alunos.utfpr.edu.br>
2020-01-12 19:36:35 -0300

    husky

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,5 @@
-/* 
+ 
 module.exports = {
     coverageDirectory: 'coverage',
     testEnvironment: 'node'
-} */
-module.exports = {
-    coverageDirectory: 'coverage',
-    testMatch: ['**/*.(spec|test).ts'],
-    testEnvironment: 'node',
-    collectCoverageFrom: ['**/src/**/*.ts', '!**/src/main/**'],
-    preset: '@shelf/jest-mongodb',
-    transform: {
-      '^.+\\.ts$': 'ts-jest'
-    }
-  }
+} 

5d1eea600c0cfe50c99b42b78c7282e892f525df
sorattorafa <soratto@alunos.utfpr.edu.br>
2020-01-13 20:03:35 -0300

    ensure AuthUseCase throws MissingParamError if no password is provided

--- src/domain/usercases/auth-usecase.spec.js
+++ src/domain/usercases/auth-usecase.spec.js
@@ -1,8 +1,11 @@
  const {MissingParamError} = require('../../utils/errors')
 class AuthUseCase { 
-    async auth(email){  
+    async auth(email, password){  
         if(!email){ 
             throw new MissingParamError('email')
+        } 
+        if(!password){ 
+            throw new MissingParamError('password')
         }
 
     }
@@ -12,5 +15,11 @@ describe('Auth use case', () => {
         const sut = new AuthUseCase() 
         const promise = sut.auth() 
         expect(promise).rejects.toThrow(new MissingParamError('email'))
-    })
+    }) 
+    test('Shold throw if no password is provided', async () => { 
+        const sut = new AuthUseCase() 
+        const promise = sut.auth('any_email@email.com') 
+        expect(promise).rejects.toThrow(new MissingParamError('password'))
+    }) 
+    
 })
